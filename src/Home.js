a7f792765f143642468008d725df8724ea57d891
Nishita Vaddem <nishitavaddem@gmail.com>
2021-01-09 13:09:41 +0530

    Use prope

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import BlogList from './BlogList'
 
 function Home() {
 
@@ -9,14 +10,14 @@ function Home() {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter((blog) => blog.id !== id)
+        setBlogs(newBlogs)
+    }
+
     return (
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={ blog.id } >
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title={ 'All Blogs' } handleDelete={handleDelete} />
         </div>
     )
 }
