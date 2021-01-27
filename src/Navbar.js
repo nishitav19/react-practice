dfec6af777eb3d6536863ccb3d3eeefab6409dae
Nishita Vaddem <nishitavaddem@gmail.com>
2021-01-27 23:17:15 +0530

    Include react router

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-    return (
-        <div>
-            <nav className="navbar">
-                <h1>The Dojo Blog</h1>
-                <div className="links">
-                    <a href="/">Home</a>
-                    <a href="/create">New Blog</a>
-                </div>
-            </nav>
+  return (
+    <div>
+      <nav className="navbar">
+        <h1>The Dojo Blog</h1>
+        <div className="links">
+          <Link to="/">Home</Link>
+          <Link to="/create">New Blog</Link>
         </div>
-    )
+      </nav>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
