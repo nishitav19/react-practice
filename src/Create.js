5e1812856fa4bafb3c5a7ae2ea6a18283f1a28e7
Nishita Vaddem <nishitavaddem@gmail.com>
2021-01-28 13:40:27 +0530

    Add input form

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,35 @@
 import React from "react";
+import { useState } from "react";
 
 function Create() {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("");
+
   return (
     <div className="create">
       <h2>Add A New Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mario">mario</option>
+          <option value="emma">emma</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 }
